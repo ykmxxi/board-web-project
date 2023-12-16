b55666ebdb166fa9620d7055215375cfb491c1b2
ykmxxi <ykmxxi97@gmail.com>
2023-12-16 13:55:31 +0900

    #7- 테스트 데이터로 db 연결과 jpa 리포지토리 기본 CRUD 테스트

---
+++ board/src/test/java/com/example/board/repository/JpaRepositoryTest.java
@@ -0,0 +1,91 @@
+package com.example.board.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import com.example.board.config.JpaConfig;
+import com.example.board.domain.Article;
+
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    JpaRepositoryTest(@Autowired final ArticleRepository articleRepository,
+                      @Autowired final ArticleCommentRepository articleCommentRepository
+    ) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("select 테스트")
+    @Test
+    void 기본_select_테스트() {
+        // given
+
+        // when
+        List<Article> articles = articleRepository.findAll();
+
+        // then
+        assertThat(articles).isNotNull()
+                .hasSize(123);
+    }
+
+    @DisplayName("insert 테스트")
+    @Test
+    void 기본_insert_테스트() {
+        // given
+        long previous = articleRepository.count();
+
+        // when
+        Article savedArticle = articleRepository.save(Article.of("new article", "new content", "new"));
+
+        // then
+        assertThat(articleRepository.count()).isEqualTo(previous + 1);
+    }
+
+    @DisplayName("update 테스트")
+    @Test
+    void 기본_update_테스트() {
+        // given
+        Article article = articleRepository.findById(1L)
+                .orElseThrow();
+        String updated = "#newHashtag";
+
+        // when
+        article.setHashtag(updated);
+        Article savedArticle = articleRepository.save(article);
+
+        // then
+        assertThat(article).extracting("hashtag").isEqualTo(updated);
+    }
+
+    @DisplayName("delete 테스트")
+    @Test
+    void 기본_delete_테스트() {
+        // given
+        Article article = articleRepository.findById(1L)
+                .orElseThrow();
+        long previousArticleCount = articleRepository.count();
+        long previousArticleCommentCount = articleCommentRepository.count();
+        int deletedCommentsSize = article.getArticleComments().size();
+
+        // when
+        articleRepository.delete(article);
+
+        // then
+        assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
+        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentsSize);
+    }
+
+}
