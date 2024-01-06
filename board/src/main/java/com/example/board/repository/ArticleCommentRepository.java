83cce43ebab47c4e1dcac80479e93fbc3c78e150
ykmxxi <ykmxxi97@gmail.com>
2024-01-06 15:27:13 +0900

    #11- 게시글, 댓글 json api를 restful 설정

--- board/src/main/java/com/example/board/repository/ArticleCommentRepository.java
+++ board/src/main/java/com/example/board/repository/ArticleCommentRepository.java
@@ -1,9 +1,11 @@
 package com.example.board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.board.domain.ArticleComment;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 
 }
--- board/src/main/java/com/example/board/repository/ArticleRepository.java
+++ board/src/main/java/com/example/board/repository/ArticleRepository.java
@@ -1,9 +1,11 @@
 package com.example.board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.board.domain.Article;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
 }
