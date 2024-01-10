de6c23aaf67322bce7aa5916105de9bbd0be9649
ykmxxi <ykmxxi97@gmail.com>
2024-01-10 12:21:33 +0900

    #17- 뷰 컨트롤러 테스트에 시큐리티 설정 추가

--- board/src/test/java/com/example/board/controller/ArticleControllerTest.java
+++ board/src/test/java/com/example/board/controller/ArticleControllerTest.java
@@ -11,10 +11,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.example.board.config.SecurityConfig;
+
 @DisplayName("View 컨트롤러 테스트 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
