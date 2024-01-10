6a9ab660c2c5ce21856db68ff8e55bd1aa3737d0
ykmxxi <ykmxxi97@gmail.com>
2024-01-10 11:25:51 +0900

    #15- 유저 계정 도메인 구현
    - 유저 엔티티가 추가된 ERD

---
+++ board/src/main/java/com/example/board/domain/UserAccount.java
@@ -0,0 +1,80 @@
+package com.example.board.domain;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "userId"),
+        @Index(columnList = "email", unique = true),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class UserAccount extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @Column(nullable = false, length = 50)
+    private String userId;
+
+    @Setter
+    @Column(nullable = false)
+    private String userPassword;
+
+    @Setter @Column(length = 100)
+    private String email;
+    @Setter @Column(length = 100)
+    private String nickname;
+    @Setter
+    private String memo;
+
+    private UserAccount(final String userId, final String userPassword, final String email,
+                        final String nickname, final String memo) {
+        this.userId = userId;
+        this.userPassword = userPassword;
+        this.email = email;
+        this.nickname = nickname;
+        this.memo = memo;
+    }
+
+    public static UserAccount of(final String userId, final String userPassword, final String email,
+                                 final String nickname, final String memo) {
+        return new UserAccount(userId, userPassword, email, nickname, memo);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UserAccount that = (UserAccount) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
