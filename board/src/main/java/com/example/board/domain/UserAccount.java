ece1f6c3ec12b7df7beaa65feecd4596e4a26fb3
ykmxxi <ykmxxi97@gmail.com>
2024-01-19 13:37:08 +0900

    #26- UserAccount 도메인의 회원 ID에 Unique Key 추가
    - 회원 ID로 로그인하고 유저를 식별하므로 uk로 설정

--- board/src/main/java/com/example/board/domain/UserAccount.java
+++ board/src/main/java/com/example/board/domain/UserAccount.java
@@ -18,7 +18,7 @@ import lombok.ToString;
 @Getter
 @ToString
 @Table(indexes = {
-        @Index(columnList = "userId"),
+        @Index(columnList = "userId", unique = true),
         @Index(columnList = "email", unique = true),
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
