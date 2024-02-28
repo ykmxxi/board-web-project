98b70fb427418fa062cacb6693f178e54aa60a6c
ykmxxi <ykmxxi97@gmail.com>
2024-02-28 10:12:26 +0900

    refactor: 테스트 전용 빈을 만들어서 넣는 방법으로 개선

--- board/src/test/java/com/example/board/controller/AuthControllerTest.java
+++ board/src/test/java/com/example/board/controller/AuthControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestComponent;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -18,7 +19,7 @@ import com.example.board.config.TestSecurityConfig;
 
 @DisplayName("View 컨트롤러 테스트 - 로그인(인증)")
 @Import(TestSecurityConfig.class)
-@WebMvcTest(Void.class)
+@WebMvcTest(AuthControllerTest.EmptyController.class)
 class AuthControllerTest {
 
     private final MockMvc mvc;
@@ -38,6 +39,13 @@ class AuthControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andDo(print());
+    }
+
+    /**
+     * 어떤 컨트롤러도 필요하지 않은 테스트임을 나타내기 위해 테스트용 빈 컴포넌트를 사용함.
+     */
+    @TestComponent
+    static class EmptyController {
     }
 
 }
